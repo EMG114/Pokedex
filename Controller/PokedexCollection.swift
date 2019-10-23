2f3e806c6a3898796048d10f15ec8425feab8a7c
Erica Gutierrez <egutierrez411@gmail.com>
2019-10-23 17:39:50 -0400

    first commit - cells not showing

---
+++ Controller/PokedexCollection.swift
@@ -0,0 +1,121 @@
+//
+//  PokedexCollection.swift
+//  Pokedex
+//
+//  Created by Erica on 10/23/19.
+//  Copyright © 2019 Erica. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "PokedexCollectionViewCell"
+
+class PokedexCollection: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+    //    self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        // Do any additional setup after loading the view.
+        configureViewComponents()
+        
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func showSearchBar() {
+        
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+    func configureViewComponents() {
+        collectionView.backgroundColor = .white
+        
+        navigationController?.navigationBar.barTintColor = .mainPink()
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barStyle = .black
+        navigationItem.title = "Pokedex"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target:self, action: #selector(showSearchBar))
+        navigationItem.rightBarButtonItem?.tintColor = .white
+        
+        collectionView.register(PokedexCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 6
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? PokedexCollectionViewCell else { return UICollectionViewCell() }
+        cell.backgroundColor = .blue
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
+
+extension PokedexCollection: UICollectionViewDelegateFlowLayout {
+    
+func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            let cellWidth = (view.frame.width - 36)/3
+            return CGSize(width:cellWidth, height: cellWidth)
+}
+
+}
---
+++ View/PokedexCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokedexCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Erica on 10/23/19.
+//  Copyright © 2019 Erica. All rights reserved.
+//
+
+import UIKit
+
+class PokedexCollectionViewCell: UICollectionViewCell {
+    
+    
+    // Mark: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
